f681340eb6c827cd275f598fb81506da3ba727de
Dor Shaked <dorsh@monday.com>
2023-04-02 11:11:56 +0300

    Fix: Change local secure storage implementation - Change usage of lowdb to using native nodejs capabilities - Adjust tests - Adjust usage in secure-storage.local.ts
    ##monday.com item id
    https://monday.monday.com/boards/3670992828/views/83352826/pulses/3875667859

--- lib/utils/local-db.ts
+++ lib/utils/local-db.ts
@@ -1,10 +1,19 @@
-import { existsSync, unlinkSync } from 'node:fs';
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import {
+  accessSync,
+  existsSync,
+  constants as fsConstants,
+  readFileSync,
+  unlinkSync,
+  writeFileSync
+} from 'node:fs';
 import { join } from 'node:path';
 
 import appRoot from 'app-root-path';
-import { Low } from 'lowdb';
-import { JSONFile } from 'lowdb/node';
 
+import { InternalServerError, NotFoundError } from 'errors/apps-sdk-error';
+import { isDefined } from 'types/guards';
+import { ILocalStorageInstance } from 'types/secure-storage.local';
 import { Logger } from 'utils/logger';
 
 const DEFAULT_DB_NAME = 'storage';
@@ -13,24 +22,108 @@ const logger = new Logger('LocalDb', { passThrough: true });
 
 const getDbPath = (dbName: string) => join(appRoot.toString(), `${dbName}.json`);
 
-export const deleteDb = (dbName = DEFAULT_DB_NAME): void => {
-  const file = getDbPath(dbName);
-  if (existsSync(file)) {
-    unlinkSync(file);
+const hasDiskWriteAccess = () => {
+  const rootDir = appRoot.toString();
+  try {
+    accessSync(rootDir, fsConstants.W_OK);
+    return true;
+  } catch (_err) {
+    return false;
   }
 };
 
-export const initDb = async (dbName = DEFAULT_DB_NAME) => {
-  const file = getDbPath(dbName);
-  logger.info(`Initializing local db in ${file}`);
+export const deleteDb = (dbName = DEFAULT_DB_NAME): void => {
+  if (hasDiskWriteAccess()) {
+    const file = getDbPath(dbName);
+    if (existsSync(file)) {
+      unlinkSync(file);
+    }
+  }
+};
+
+const inMemoryData: Record<string, any> = {};
+
+export class LocalMemoryDb implements ILocalStorageInstance {
+  async set<T>(key: string, value: T): Promise<boolean> {
+    inMemoryData[key] = value;
+    return Promise.resolve(true);
+  }
   
-  const adapter = new JSONFile(file);
-  const db = new Low(adapter);
+  async delete(key: string): Promise<boolean> {
+    delete inMemoryData[key];
+    return Promise.resolve(true);
+  }
   
-  await db.read();
-  db.data ||= {};
-  await db.write();
-  logger.info('initialized local db');
+  async get<T>(key: string): Promise<T> {
+    if (key in inMemoryData) {
+      return Promise.resolve(inMemoryData[key] as T);
+    }
+    
+    throw new NotFoundError(`Data not found for '${key}'`);
+  }
+}
+
+export class LocalDb implements ILocalStorageInstance {
+  private readonly dbFilePath: string;
+  private memoryData: Record<string, any>;
   
-  return db;
+  constructor(dbFileName: string = DEFAULT_DB_NAME) {
+    if (!hasDiskWriteAccess()) {
+      throw new InternalServerError('Missing write permissions');
+    }
+    
+    this.dbFilePath = getDbPath(dbFileName);
+    if (!existsSync(this.dbFilePath)) {
+      this.memoryData = {};
+      writeFileSync(this.dbFilePath, JSON.stringify(this.memoryData), { encoding: 'utf8', flag: 'wx' });
+      return;
+    }
+    
+    const stringifiedDbData = readFileSync(this.dbFilePath, 'utf-8');
+    if (isDefined(stringifiedDbData)) {
+      this.memoryData = JSON.parse(stringifiedDbData);
+      return;
+    }
+    
+    this.memoryData = {};
+  }
+  
+  async set<T>(key: string, value: T): Promise<boolean> {
+    this.memoryData[key] = value;
+    
+    writeFileSync(this.dbFilePath, JSON.stringify(this.memoryData));
+    return Promise.resolve(true);
+  }
+  
+  async delete(key: string): Promise<boolean> {
+    delete this.memoryData[key];
+    
+    writeFileSync(this.dbFilePath, JSON.stringify(this.memoryData));
+    return Promise.resolve(true);
+  }
+  
+  async get<T>(key: string): Promise<T> {
+    if (key in this.memoryData) {
+      return Promise.resolve(this.memoryData[key] as T);
+    }
+    
+    const data = readFileSync(this.dbFilePath, 'utf-8');
+    const parsedData: Record<string, any> = JSON.parse(data);
+    this.memoryData = parsedData;
+    if (key in this.memoryData) {
+      return Promise.resolve(this.memoryData[key] as T);
+    }
+    
+    throw new NotFoundError(`Data not found for '${key}'`);
+  }
+}
+
+export const initDb = (dbName = DEFAULT_DB_NAME) => {
+  if (hasDiskWriteAccess()) {
+    logger.info('Initializing local db');
+    return new LocalDb(dbName);
+  }
+  
+  logger.info('No disk access, initializing in memory db');
+  return new LocalMemoryDb();
 };
