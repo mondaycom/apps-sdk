796561e5b1952cde8490b41bef9564950c692d77
shay <shay.elkana@gmail.com>
2023-11-13 18:29:41 +0200

    feature: added support for sending queue messages.

--- lib/index.ts
+++ lib/index.ts
@@ -1,8 +1,8 @@
 import { EnvironmentVariablesManager } from 'lib/environment-variables-manager';
 import { Logger } from 'lib/logger';
+import { Queue } from 'lib/queue';
 import { SecureStorage } from 'lib/secure-storage';
 import { Storage } from 'lib/storage';
-import { Queue } from 'lib/queue';
 
 export {
   SecureStorage,
--- lib/queue/queue.ts
+++ lib/queue/queue.ts
@@ -1,7 +1,8 @@
+import { PubSub } from '@google-cloud/pubsub';
+
 import {BadRequestError, InternalServerError} from 'errors/apps-sdk-error';
+import { IQueue } from 'types/queue';
 import { Logger } from 'utils/logger';
-import { IQueue } from "types/queue";
-import { PubSub } from "@google-cloud/pubsub";
 
 const logger = new Logger('Queue', { mondayInternal: true });
 
@@ -13,14 +14,14 @@ export class Queue implements IQueue {
 
     async publishMessage(topicName: string, message: (Uint8Array|string|null)): Promise<string> {
         if (!topicName) {
-            throw new BadRequestError("topicName is missing or empty.");
+            throw new BadRequestError('topicName is missing or empty.');
         }
 
         try {
 
             const messageId = await this.pubSubClient
                 .topic(topicName)
-                .publishMessage({data: message, attributes: {"Content-Type": "application/json"}});
+                .publishMessage({data: message, attributes: {'Content-Type': 'application/json'}});
             return messageId;
 
         } catch (err) {
@@ -28,7 +29,7 @@ export class Queue implements IQueue {
             if (err  instanceof Error) {
                 throw new BadRequestError(err.message);
             } else {
-                throw new InternalServerError("An error occurred while sending message toe queue.")
+                throw new InternalServerError('An error occurred while sending message toe queue.')
             }
         }
     }
