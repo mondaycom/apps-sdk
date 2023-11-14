46d6698795a34abdeaacbee11d7a68cc5fae5584
shay <shay.elkana@gmail.com>
2023-11-14 18:33:00 +0200

    feature: support sdk queue local

--- lib/queue/queue.ts
+++ lib/queue/queue.ts
@@ -1,54 +1,29 @@
 import { PubSub } from '@google-cloud/pubsub';
-import fetch from 'node-fetch';
 
 import {BadRequestError, InternalServerError} from 'errors/apps-sdk-error';
 import { IQueue } from 'types/queue';
-import {isLocalEnvironment, localServerAddress} from 'utils/env';
 import { Logger } from 'utils/logger';
 
 const logger = new Logger('Queue', { mondayInternal: true });
-const devQueueSecret = '376a573e97497a294fd50e584fa3e507d1eab65abd2019709c0e8dc6b18932fc'
-const devQueueTopic = 'localQueue'
-const devGenerateMessageId = () => {
-    let messageId = '';
-    while (messageId.length < 16) {
-        messageId += Math.floor(Math.random() * (9 - 1 + 1) + 1).toString()
-    }
-    return messageId;
-}
 
-export class Queue implements IQueue {
+export class QueueProd implements IQueue {
     private pubSubClient: PubSub;
     constructor() {
         this.pubSubClient = new PubSub();
     }
 
     async publishMessage(message: (Uint8Array|string), options?: { topicName: string }): Promise<string> {
-        const topicName = options?.topicName || process.env.MNDY_TOPIC_NAME || isLocalEnvironment() ? devQueueTopic : '';
+        const topicName = options?.topicName || process.env.MNDY_TOPIC_NAME;
         if (!topicName) {
             throw new BadRequestError('topicName is missing or empty.');
         }
 
         try {
-            if (isLocalEnvironment()) {
-                const payload = (message.toString) ? message.toString() : message;
-                const serverAddress = localServerAddress();
-                // eslint-disable-next-line @typescript-eslint/no-floating-promises
-                fetch(`${serverAddress}/mndy-queue?secret=${devQueueSecret}`, {
-                    'headers': {
-                        'content-type': 'application/json',
-                    },
-                    'body': payload,
-                    'method': 'POST'
-                });
-                return devGenerateMessageId();
-            } else {
-                const data = (typeof message === 'string' || message instanceof String) ? Buffer.from(message) : message;
-                const messageId = await this.pubSubClient
-                    .topic(topicName)
-                    .publishMessage({data, attributes: {'Content-Type': 'application/json'}});
-                return messageId;
-            }
+            const data = (typeof message === 'string' || message instanceof String) ? Buffer.from(message) : message;
+            const messageId = await this.pubSubClient
+                .topic(topicName)
+                .publishMessage({data, attributes: {'Content-Type': 'application/json'}});
+            return messageId;
         } catch (err) {
             logger.error(JSON.stringify(err));
             throw new InternalServerError('An error occurred while sending message toe queue.')
@@ -56,7 +31,7 @@ export class Queue implements IQueue {
     }
 
     validateMessageOrigin(secret: string) : boolean {
-        const envMessageSecret = process.env.MNDY_TOPIC_MESSAGES_SECRET || isLocalEnvironment() ? devQueueSecret : '';
+        const envMessageSecret = process.env.MNDY_TOPIC_MESSAGES_SECRET;
         if (!envMessageSecret) {
             throw new BadRequestError('En environment variable name "MNDY_SERVER_ADDRESS" is required.');
         }
