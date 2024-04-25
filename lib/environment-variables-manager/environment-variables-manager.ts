9c3b4becb4069b739236b615831225b974ea3dd6
Maor Barazani <maorba@monday.com>
2024-04-26 00:28:17 +0300

    create secrets manager seperate files remove env-specific logic

--- lib/environment-variables-manager/environment-variables-manager.ts
+++ lib/environment-variables-manager/environment-variables-manager.ts
@@ -3,9 +3,9 @@ import * as process from 'process';
 
 import { JsonValue } from 'lib/types/general';
 import {
-  EnvironmentData,
   GetOptions,
-  IEnvironmentVariablesManager,
+  IKeyValueManager,
+  KeyValueData,
   Options
 } from 'types/environment-variables-manager';
 import { isDefined } from 'types/guards';
@@ -21,27 +21,27 @@ const readEnvironmentData = () => {
     logger.error('[EnvironmentVariablesManager] Environment file name is not defined');
     return;
   }
-  
+
   const environmentDataFilePath = `/secrets/${environmentDataFileName}`;
   const isEnvironmentFileExists = fs.existsSync(environmentDataFilePath);
   if (!isEnvironmentFileExists) {
     logger.error('[EnvironmentVariablesManager] Environment file does not exist in path');
     return;
   }
-  
+
   const environmentData = fs.readFileSync(environmentDataFilePath, 'utf8');
   try {
-    return JSON.parse(environmentData) as EnvironmentData;
+    return JSON.parse(environmentData) as KeyValueData;
   } catch (error) {
     logger.error('[EnvironmentVariablesManager] Corrupted Environment file. File is not in JSON format');
     return;
   }
 };
 
-export class EnvironmentVariablesManager implements IEnvironmentVariablesManager {
-  private cachedEnvironmentData?: EnvironmentData;
+export class EnvironmentVariablesManager implements IKeyValueManager {
+  private cachedEnvironmentData?: KeyValueData;
   private shouldUpdateProcessEnv: boolean;
-  
+
   constructor(options?: Options) {
     this.shouldUpdateProcessEnv = !!options?.updateProcessEnv;
     this.initEnv(options);
@@ -49,54 +49,54 @@ export class EnvironmentVariablesManager implements IEnvironmentVariablesManager
       logger.info('[EnvironmentVariablesManager] Running in development environment, Using process.env', { mondayInternal: false });
     }
   }
-  
+
   private initEnvIfNeeded(options?: GetOptions) {
     const shouldInvalidate = options?.invalidate ?? true;
     if (shouldInvalidate || !this.cachedEnvironmentData) {
       this.initEnv();
     }
   }
-  
+
   private initEnv(options: Options = {}) {
     if (isLocalEnvironment()) {
-      this.cachedEnvironmentData = process.env as EnvironmentData;
+      this.cachedEnvironmentData = process.env as KeyValueData;
       return;
     }
-    
+
     const { updateProcessEnv } = options;
     this.shouldUpdateProcessEnv = updateProcessEnv ?? this.shouldUpdateProcessEnv;
     this.cachedEnvironmentData = readEnvironmentData();
     if (!isDefined(this.cachedEnvironmentData)) {
       return;
     }
-    
+
     if (this.shouldUpdateProcessEnv) {
       Object.entries(this.cachedEnvironmentData).forEach(([key, value]) => {
         const snakeCaseKey = snakeCase(key, { upperCase: true });
         process.env[snakeCaseKey] = value as string;
       });
     }
-    
+
     logger.info('[EnvironmentVariablesManager] Initialized environment variables data', { mondayInternal: true });
   }
-  
+
   getKeys(options?: GetOptions): Array<string> {
     this.initEnvIfNeeded(options);
     if (!isDefined(this.cachedEnvironmentData)) {
       logger.error('[EnvironmentVariablesManager.getKeys] There is an issue with loading keys', { mondayInternal: false });
       return [];
     }
-    
+
     return Object.keys(this.cachedEnvironmentData);
   }
-  
+
   get(key: string, options?: GetOptions): JsonValue {
     this.initEnvIfNeeded(options);
     if (!isDefined(this.cachedEnvironmentData)) {
       logger.error('[EnvironmentVariablesManager.get] There is an issue with loading data for key', { mondayInternal: false });
       return null;
     }
-    
+
     return this.cachedEnvironmentData[key] || process.env[key] as JsonValue;
   }
 }
---
+++ lib/secrets-manager/secrets-manager.ts
@@ -0,0 +1,89 @@
+import fs from 'fs';
+import * as process from 'process';
+
+import { JsonValue } from 'lib/types/general';
+import {
+  GetOptions,
+  IKeyValueManager,
+  KeyValueData,
+} from 'types/environment-variables-manager';
+import { isDefined } from 'types/guards';
+import { isLocalEnvironment } from 'utils/env';
+import { Logger } from 'utils/logger';
+
+const logger = new Logger('SecretsManager', { mondayInternal: true });
+
+// TODO: write tests, just like env-var-manager has
+// TODO: add readme for how to use this
+// TODO: bump version
+
+const readSecretsData = () => {
+  const secretsDataFileName = process?.env?.SECRET_NAME; // TODO this should change for either secrets, or env-vars
+  if (!isDefined(secretsDataFileName)) {
+    logger.error('[SecretsManager] Secrets file name is not defined');
+    return;
+  }
+
+  const secretsDataFilePath = `/secrets/${secretsDataFileName}`; // TODO: this stays the same for secrets and envs, the /secrets as the parent folder?
+  const isSecretsFileExists = fs.existsSync(secretsDataFilePath);
+  if (!isSecretsFileExists) {
+    logger.error('[SecretsManager] Secrets file does not exist in path');
+    return;
+  }
+
+  const secretsData = fs.readFileSync(secretsDataFilePath, 'utf8');
+  try {
+    return JSON.parse(secretsData) as KeyValueData;
+  } catch (error) {
+    logger.error('[SecretsManager] Corrupted Secrets file. File is not in JSON format');
+    return;
+  }
+};
+
+export class SecretsManager implements IKeyValueManager {
+  private cachedSecretsData?: KeyValueData;
+
+  constructor() {
+    this.initSecrets();
+    if (isLocalEnvironment()) {
+      logger.info('[SecretsManager] Running in development environment, Using process.env', { mondayInternal: false });
+    }
+  }
+
+  private initSecretsIfNeeded(options?: GetOptions) {
+    const shouldInvalidate = options?.invalidate ?? true;
+    if (shouldInvalidate || !this.cachedSecretsData) {
+      this.initSecrets();
+    }
+  }
+
+  private initSecrets() {
+    if (isLocalEnvironment()) {
+      this.cachedSecretsData = process.env as KeyValueData;
+      return;
+    }
+
+    this.cachedSecretsData = readSecretsData();
+    logger.info('[SecretsManager] Initialized secrets data', { mondayInternal: true });
+  }
+
+  getKeys(options?: GetOptions): Array<string> {
+    this.initSecretsIfNeeded(options);
+    if (!isDefined(this.cachedSecretsData)) {
+      logger.error('[SecretsManager.getKeys] There is an issue with loading keys', { mondayInternal: false });
+      return [];
+    }
+
+    return Object.keys(this.cachedSecretsData);
+  }
+
+  get(key: string, options?: GetOptions): JsonValue {
+    this.initSecretsIfNeeded(options);
+    if (!isDefined(this.cachedSecretsData)) {
+      logger.error('[SecretsManager.get] There is an issue with loading data for key', { mondayInternal: false });
+      return null;
+    }
+
+    return this.cachedSecretsData[key] || process.env[key] as JsonValue;
+  }
+}
