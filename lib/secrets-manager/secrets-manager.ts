15760943435a33fce6ece42191bbd0f85edd4c91
Maor Barazani <maorba@monday.com>
2024-05-01 13:55:29 +0300

    remove comment

--- lib/secrets-manager/secrets-manager.ts
+++ lib/secrets-manager/secrets-manager.ts
@@ -1,8 +1,6 @@
 import { KeyValueManager } from 'lib/key-value-manager/key-value-manager.abstract';
 import { IKeyValueManager } from 'types/key-value-manager';
 
-// TODO: create new release on github -  with notes on env-vars not being secured anymore
-
 export class SecretsManager extends KeyValueManager implements IKeyValueManager {
   constructor() {
     const dataFileName = process.env.SECRET_NAME!;
